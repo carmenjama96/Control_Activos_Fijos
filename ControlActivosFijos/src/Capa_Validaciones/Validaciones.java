32afb60c0c5888036d9a68c294098d3eea646fa5
Anita Maria Lazo Alay <AnitaLazo@users.noreply.github.com>
2016-09-08 21:45:20 -0500

    Create Validaciones.java

---
+++ ControlActivosFijos/src/Capa_Validaciones/Validaciones.java
@@ -0,0 +1,93 @@
+
+public class Validaciones {
+    //Esta funcion se utiliza para validar tanto modelo como serie en activos
+    public static String validarmayusculasynumeros(String texto){
+        String validos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        int i,j,coinci = 0;
+        String cadena = texto;
+        String mensaje="";
+        for (j = 0; j <= cadena.length(); j++) {
+            for (i = 0; i < validos.length(); i++) {
+
+                if (cadena.charAt(j) == validos.charAt(i)) {
+                    coinci++;
+
+                }
+
+            }
+
+        }
+        if (cadena.length() == coinci) {
+             System.out.println("todos los caracteres son validos");
+             mensaje="caracteres correctos";
+         } else {
+             System.out.println("hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres" + "  " + validos);
+             mensaje="hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres "+validos;
+           
+        }
+         return mensaje;
+    }
+    //Esta funcion se utiliza para validar el codigo de oficio interno
+    public static String validarmaynumguion(String texto){
+        String validos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+
+        int i,j,coinci = 0;
+        String cadena = texto;
+        String mensaje="";
+        for (j = 0; j <= cadena.length(); j++) {
+            for (i = 0; i < validos.length(); i++) {
+
+                if (cadena.charAt(j) == validos.charAt(i)) {
+                    coinci++;
+
+                }
+
+            }
+
+        }
+        if (cadena.length() == coinci) {
+             System.out.println("todos los caracteres son validos");
+            mensaje="caracteres correctos";
+            
+        } else {
+            
+            System.out.println("hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres" + "  " + validos);
+            mensaje="hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres "+validos;
+           
+        }
+         return mensaje;
+    }
+    //Esta funcion se utiliza para validar el codigo institucional
+    public static String validarmayminnumguion(String texto){
+        String validos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-";
+
+        int i,j,coinci = 0;
+        String cadena = texto;
+        String mensaje="";
+        for (j = 0; j <= cadena.length(); j++) {
+            for (i = 0; i < validos.length(); i++) {
+
+                if (cadena.charAt(j) == validos.charAt(i)) {
+                    coinci++;
+
+                }
+
+            }
+
+        }
+        if (cadena.length() == coinci) {
+             System.out.println("todos los caracteres son validos");
+            mensaje="caracteres correctos";
+            
+        } else {
+            
+            System.out.println("hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres" + "  " + validos);
+            mensaje="hay al menos algún caracter que no es valido. Solo se consideran validos los siguientes carecteres "+validos;
+           
+        }
+         return mensaje;
+    }
+    
+    
+}
