9fb95cce5da677be21305f9fa4443f3a2aaea795
Kevin Leandro Muñiz Ponce <Mariaca del espacio@Sony>
2016-09-20 12:37:50 -0500

    Eliminar eventos que no son necesarios

--- ControlActivosFijos/src/Capa_Negocio/NewClass.java
+++ ControlActivosFijos/src/Capa_Negocio/NewClass.java
@@ -2,15 +2,4 @@ package Capa_Negocio;
 
 public class NewClass {
 
-    static String id_ActivosSeleccionados;
-    
-    public static void ingresarIdsActivos(String ia)
-    {
-        id_ActivosSeleccionados=ia;
-    }
-    
-    public static String returnIdsActivos()
-    {
-        return  id_ActivosSeleccionados;
-    }
 }
